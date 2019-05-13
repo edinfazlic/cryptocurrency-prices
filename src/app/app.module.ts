249388b3d136cfa088ec683599ad83a4bc35d475
Edin Fazlic <edin.fazlic@serapion-it.com>
2019-05-13 18:32:26 +0200

    Added: settings page and menu toolbar with routing to settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule} from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -12,13 +15,17 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CryptocurrencyListComponent} from './cryptocurrency-list/cryptocurrency-list.component';
 import {CryptocurrencyDetailsComponent} from './cryptocurrency-details/cryptocurrency-details.component';
 import {DetailsListItemComponent} from './cryptocurrency-details/details-list-item/details-list-item.component';
+import {MenuToolbarComponent} from './menu-toolbar/menu-toolbar.component';
+import {SettingsComponent} from './settings/settings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CryptocurrencyListComponent,
     CryptocurrencyDetailsComponent,
-    DetailsListItemComponent
+    DetailsListItemComponent,
+    MenuToolbarComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,10 @@ import {DetailsListItemComponent} from './cryptocurrency-details/details-list-it
     MatPaginatorModule,
     MatTooltipModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatToolbarModule,
+    FormsModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
