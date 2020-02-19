ac2b8242305e87683112d4865030eaa118e47781
Edin Fazlic <edin.fazlic@gmail.com>
2020-02-19 23:42:44 +0100

    Changed: test api key set on prod. Output folder set to be just dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '',
-  apiKey: ''
+  apiUrl: 'https://sandbox-api.coinmarketcap.com',
+  apiKey: '457b7c07-8a9a-4299-9325-31d40869126d'
 };
